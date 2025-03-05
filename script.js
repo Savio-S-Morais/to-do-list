ebdbe8ed621f60b55df0ae69476f73629051d65a
Sávio Morais <saviosam09@gmail.com>
2025-03-05 16:30:43 -0300

    DOCS: Add JavaScript functions

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-// Here will have functions for add, delete and check tasks
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addTask');
+const taskList = document.getElementById('taskList');
+
+loadTasks();
+
+function addTask() {
+    const task = taskInput.value.trim();
+
+    if(task === '') {
+        alert('Please enter a task!')
+    } else {
+        createTaskElement(task);
+        saveTasks();
+        taskInput.value = '';
+    }
+};
+
+addButton.addEventListener('click', addTask);
+
+function createTaskElement(task) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = task;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteTask';
+
+    listItem.appendChild(deleteButton);
+    taskList.appendChild(listItem);
+
+    deleteButton.addEventListener('click', function(){
+        taskList.removeChild(listItem);
+        saveTasks();
+    });
+
+};
+
+function saveTasks() {
+    let tasks = [];
+    taskList.querySelectorAll('li').forEach(function(item) {
+        tasks.push(item.textContent.replace('Delete', '').trim());
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    tasks.forEach(createTaskElement);
+};
